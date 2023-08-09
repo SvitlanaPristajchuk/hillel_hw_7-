456186fee29cf32bb26a40d5eea8c9ba825b9d31
Svitlana Pristajchuk <svetlana.pristajchuk@gmail.com>
2023-08-09 12:46:27 +0200

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+sports = [
+    ['skier', '⛷'],
+    ['snowboarder', '🏂'],
+    ['apple', '🍎'],
+    ['hockey', '🏒'],
+    ['ice skate', '⛸'],
+    ['swimmer', '🏊'],
+    ['surfer', '🏄‍'],
+    ['watermelon', '🍉'],
+    ['lemon', '🍋'],
+    ['rowboat', '🚣'],
+    ['bicyclist', '🚴‍']
+];
+console.log(sports)
+
+summer_sports = []
+console.log(summer_sports)
+
+summer_sports = sports.slice(2)
+console.log(summer_sports)
