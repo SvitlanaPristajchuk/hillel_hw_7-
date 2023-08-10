aa93193d55099759732ddaa40672a37e2e13dbd2
Svitlana Pristajchuk <svetlana.pristajchuk@gmail.com>
2023-08-10 19:24:25 +0200

    add

--- index.js
+++ index.js
@@ -11,10 +11,58 @@ sports = [
     ['rowboat', '🚣'],
     ['bicyclist', '🚴‍']
 ];
-console.log(sports)
 
-summer_sports = []
-console.log(summer_sports)
+firstSpotrs = sports.slice()
+firstSpotrs = sports.slice(5, 7);
+console.log(firstSpotrs)
 
-summer_sports = sports.slice(0, 2)
-console.log(summer_sports)
+secondSummerSpors = sports.slice()
+secondSummerSpors = sports.slice(9, 12)
+console.log(secondSummerSpors)
+
+summerSports = firstSpotrs.concat(secondSummerSpors)
+console.log(summerSports)
+
+firstWinterSport = sports.slice()
+console.log(firstWinterSport)
+firstWinterSport = sports.slice(0, 2)
+console.log(firstWinterSport)
+
+secondWinterSport = sports.slice()
+console.log(secondWinterSport)
+secondWinterSport = sports.slice(3, 5)
+console.log(secondWinterSport)
+
+winterSports = firstWinterSport.concat(secondWinterSport)
+console.log(winterSports)
+
+firstFructGroup = sports.slice()
+console.log(firstFructGroup)
+
+firstFructGroup = sports.slice(2, 3)
+console.log(firstFructGroup)
+
+secondFructGroup = sports.slice(7, 9)
+console.log(secondFructGroup)
+
+fruits = firstFructGroup.concat(secondFructGroup)
+console.log(fruits)
+
+listFruits = fruits.join(`</li> <li>`);
+listWinterSports = winterSports.join(`</li><li>`)
+listSummerSports = summerSports.join(`</li> <li>`)
+
+
+
+
+document.write(`
+
+<ul>
+<p>Winter Sports</p>
+<li>${listWinterSports}</li>
+<p> Summer Sports </p>
+<li>${listSummerSports}</li>
+<p> Fruits </p>
+<li> ${listFruits} </li>
+</ul>
+`)
